0f64d95dbe486bac2b32097ad9b638af69f41a48
shermszz <tshshermanz@gmail.com>
2026-01-20 21:37:56 +0800

    Level-3: Add Mark as Done Feature

--- src/main/java/Shermszz.java
+++ src/main/java/Shermszz.java
@@ -2,50 +2,115 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Shermszz {
-    private static ArrayList<String> list; // To store a list of user's instructions
+    private static ArrayList<Task> list; // To store a list of user's tasks.
 
     private static void printLine() {
         System.out.println("--------------------------------------");
     }
 
-    private static void printList() {
-        int i = 1;
-        if (list.isEmpty()) {
-            System.out.println("The list is empty. Add something...");
-            printLine();
+    private static void greetUser() {
+        printLine();
+        System.out.println("Hello! I'm Shermszz");
+        System.out.println("What can I do for you?\n");
+        printLine();
+    }
+
+    private static void sayBye() {
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!\n");
+        printLine();
+    }
+
+    private static void listTasks() {
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i + 1) + ". " + list.get(i).toString());
         }
-        for (String s : list) {
-            System.out.println(i + ". " + s);
-            i++;
+    }
+
+    private static void markTask(String command) {
+        try {
+            String[] parts = command.split(" "); //Should be mark X, where X is a number
+            int idx = Integer.parseInt(parts[1]);
+            if (idx > list.size() || idx < 1) {
+                System.out.println("Cannot mark task with ID = " + idx + ". It does not exist.");
+            } else {
+                //Within range, so we mark the task
+                Task t = list.get(idx - 1); //0 -based indexing
+                if (t.getStatusIcon().equals("X")) {
+                    System.out.println("This task is already marked as completed.");
+                } else {
+                    t.markAsDone(); //Set to completed
+                    System.out.println("Nice! I've marked this task as done:");
+                }
+                System.out.println(t.toString());
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("You did no enter an appropriate format. Please enter 'mark X', where X is a digit.");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("You did no enter an appropriate format. Please enter 'mark X', where X is a digit.");
         }
-        System.out.println(); //Just for spacing
+
+
+    }
+
+    private static void unmarkTask(String command) {
+        try {
+            String[] parts = command.split(" "); //Should be mark X, where X is a number
+            int idx = Integer.parseInt(parts[1]);
+            if (idx > list.size() || idx < 1) {
+                System.out.println("Cannot unmark task with ID =  " + idx + ". It does not exist.");
+            } else {
+                //Within range, so we mark the task
+                Task t = list.get(idx - 1); //0 -based indexing
+                if (t.getStatusIcon().equals(" ")) {
+                    //Already an unmarked task, inform the user
+                    System.out.println("This task has yet to be completed.");
+                } else {
+                    t.markAsIncomplete(); //Set to incomplete now
+                    System.out.println("Ok, I've marked this task as not done yet:");
+                }
+                System.out.println(t.toString());
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("You did no enter an appropriate format. Please enter 'unmark X', where X is a digit.");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("You did no enter an appropriate format. Please enter 'unmark X', where X is a digit.");
+        }
+
+    }
+
+    private static void addTask(String command) {
+        printLine();
+        Task t = new Task(command);
+        list.add(t);
+        System.out.println("Shermszz added your instruction '"  + command + "' to your list of tasks");
+        printLine();
     }
 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         list = new ArrayList<>();
-        printLine();
-        System.out.println("Hello! I'm Shermszz");
-        System.out.println("What can I do for you?\n");
-        printLine();
+        greetUser();
 
         while (true) {
-            String echo = sc.nextLine();
-            if (echo.equals("bye")) break;
-            else if (echo.equals("list"))  {
-                printList();
+            String instruction = sc.nextLine();
+            if (instruction.equals("bye")) break;
+            else if (instruction.equals("list")) {
+                listTasks();
+            } else if (instruction.startsWith("mark")) { // Command to mark some instruction
+                markTask(instruction);
+            } else if (instruction.startsWith("unmark")) {
+                unmarkTask(instruction);
             }
             else {
-                printLine();
-                list.add(echo);
-                System.out.println("Shermszz added your command: " + echo);
-                printLine();
+                //Default adding instructions
+                addTask(instruction);
             }
         }
 
-        printLine();
-        System.out.println("Bye. Hope to see you again soon!\n");
-        printLine();
+        sayBye();
+        sc.close();
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Task {
+    private boolean completed;
+    private String description;
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+
+    @Override
+    public String toString() {
+        if (this.completed) {
+            return "[X] " + this.description;
+        }
+        return "[] " + this.description;
+    }
+
+    public String getStatusIcon() {
+        return completed ? "X" : " ";
+    }
+
+    public void markAsDone() {
+        this.completed = true;
+    }
+
+    public void markAsIncomplete() {
+        this.completed = false;
+    }
+}
