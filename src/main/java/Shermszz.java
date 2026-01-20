75931ef36cd885d6834315f64e487302effd76c2
shermszz <tshshermanz@gmail.com>
2026-01-20 18:39:08 +0800

    Level-0: Rename to Shermszz, Greet and Exit

---
+++ src/main/java/Shermszz.java
@@ -0,0 +1,12 @@
+public class Shermszz {
+    public static void main(String[] args) {
+        String horizontalLine = "--------------------------------------";
+        System.out.println(horizontalLine);
+        System.out.println("Hello! I'm Shermszz");
+        System.out.println("What can I do for you?\n");
+        System.out.println(horizontalLine);
+
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println(horizontalLine);
+    }
+}
